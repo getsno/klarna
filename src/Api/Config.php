affd2c0ee68a1a4d07f5a467dbdb429dcac4be50
Alex <alexandr.azartsev@gmail.com>
2022-01-19 12:18:18 +0200

    Change test/live to dev/prod

--- src/Api/Config.php
+++ src/Api/Config.php
@@ -5,13 +5,13 @@ namespace Gets\Klarna\Api;
 class Config
 {
     public array $baseUrls = [
-        'test' => 'https://api.playground.klarna.com/',
-        'live' => 'https://api.klarna.com/'
+        'dev' => 'https://api.playground.klarna.com/',
+        'prod' => 'https://api.klarna.com/'
     ];
 
     public string $baseUrl;
 
-    public function __construct(public $user, public $password, $mode = 'test')
+    public function __construct(public $user, public $password, $mode = 'dev')
     {
         $this->baseUrl = $this->baseUrls[$mode];
     }
